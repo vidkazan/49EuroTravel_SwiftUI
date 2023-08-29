c39bcac7aeb92d2c54fe77e4c93dd28736b3c331
Dmitrii Grigorev <vid.kazan@gmail.com>
2023-08-29 02:06:02 +0200

    getting scrollview position

--- Chew-chew-SwiftUI/View/journey/JourneyCellView.swift
+++ Chew-chew-SwiftUI/View/journey/JourneyCellView.swift
@@ -7,109 +7,12 @@
 
 import SwiftUI
 
-struct ScrollViewGestureButtonStyle: ButtonStyle {
-
-	init(
-		pressAction: @escaping () -> Void,
-		doubleTapTimeoutout: TimeInterval,
-		doubleTapAction: @escaping () -> Void,
-		longPressTime: TimeInterval,
-		longPressAction: @escaping () -> Void,
-		endAction: @escaping () -> Void
-	) {
-		self.pressAction = pressAction
-		self.doubleTapTimeoutout = doubleTapTimeoutout
-		self.doubleTapAction = doubleTapAction
-		self.longPressTime = longPressTime
-		self.longPressAction = longPressAction
-		self.endAction = endAction
-	}
-
-	private var doubleTapTimeoutout: TimeInterval
-	private var longPressTime: TimeInterval
-
-	private var pressAction: () -> Void
-	private var longPressAction: () -> Void
-	private var doubleTapAction: () -> Void
-	private var endAction: () -> Void
-
-	@State
-	var doubleTapDate = Date()
-
-	@State
-	var longPressDate = Date()
-
-	func makeBody(configuration: Configuration) -> some View {
-		configuration.label
-			.onChange(of: configuration.isPressed) { isPressed in
-				longPressDate = Date()
-				if isPressed {
-					pressAction()
-					doubleTapDate = tryTriggerDoubleTap() ? .distantPast : .now
-					tryTriggerLongPressAfterDelay(triggered: longPressDate)
-				} else {
-					endAction()
-				}
-			}
-	}
-}
-
-private extension ScrollViewGestureButtonStyle {
-
-	func tryTriggerDoubleTap() -> Bool {
-		let interval = Date().timeIntervalSince(doubleTapDate)
-		guard interval < doubleTapTimeoutout else { return false }
-		doubleTapAction()
-		return true
-	}
-
-	func tryTriggerLongPressAfterDelay(triggered date: Date) {
-		DispatchQueue.main.asyncAfter(deadline: .now() + longPressTime) {
-			guard date == longPressDate else { return }
-			longPressAction()
-		}
-	}
-}
-
-struct ScrollViewGestureButton<Label: View>: View {
-
-	init(
-		doubleTapTimeoutout: TimeInterval = 0.5,
-		longPressTime: TimeInterval = 1,
-		pressAction: @escaping () -> Void = {},
-		releaseAction: @escaping () -> Void = {},
-		endAction: @escaping () -> Void = {},
-		longPressAction: @escaping () -> Void = {},
-		doubleTapAction: @escaping () -> Void = {},
-		label: @escaping () -> Label
-	) {
-		self.style = ScrollViewGestureButtonStyle(
-			pressAction: pressAction,
-			doubleTapTimeoutout: doubleTapTimeoutout,
-			doubleTapAction: doubleTapAction,
-			longPressTime: longPressTime,
-			longPressAction: longPressAction,
-			endAction: endAction
-			)
-		self.releaseAction = releaseAction
-		self.label = label
-	}
-
-	var label: () -> Label
-	var style: ScrollViewGestureButtonStyle
-	var releaseAction: () -> Void
-
-	var body: some View {
-		Button(action: releaseAction, label: label)
-			.buttonStyle(style)
-	}
-}
-
-
 struct JourneyCellView: View {
+	@State var scrollOffset = CGFloat.zero
 	@EnvironmentObject var viewModel : SearchLocationViewModel
     var body: some View {
 		VStack{
+			Text("\(scrollOffset)")
 			switch viewModel.resultJourneysCollectionViewDataSourse.awaitingData {
 			case true:
 				VStack{
@@ -122,7 +25,7 @@ struct JourneyCellView: View {
 				.transition(.identity)
 				.animation(.interactiveSpring(), value: viewModel.resultJourneysCollectionViewDataSourse)
 			case false:
-				ScrollView(showsIndicators: false) {
+				ObservableScrollView(scrollOffset: $scrollOffset) { _ in
 					if viewModel.state == .onNewDataJourney {
 						ForEach(viewModel.resultJourneysCollectionViewDataSourse.journeys) { journey in
 							VStack {
@@ -150,4 +53,46 @@ struct JourneyView_Previews: PreviewProvider {
     static var previews: some View {
         JourneyCellView()
     }
+}
+
+
+// Simple preference that observes a CGFloat.
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+  static var defaultValue = CGFloat.zero
+
+  static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+	value += nextValue()
+  }
+}
+
+// A ScrollView wrapper that tracks scroll offset changes.
+struct ObservableScrollView<Content>: View where Content : View {
+  @Namespace var scrollSpace
+
+  @Binding var scrollOffset: CGFloat
+  let content: (ScrollViewProxy) -> Content
+
+  init(scrollOffset: Binding<CGFloat>,
+	   @ViewBuilder content: @escaping (ScrollViewProxy) -> Content) {
+	_scrollOffset = scrollOffset
+	self.content = content
+  }
+
+  var body: some View {
+	ScrollView(showsIndicators: false) {
+	  ScrollViewReader { proxy in
+		content(proxy)
+		  .background(GeometryReader { geo in
+			  let offset = -geo.frame(in: .named(scrollSpace)).minY
+			  Color.clear
+				.preference(key: ScrollViewOffsetPreferenceKey.self,
+							value: offset)
+		  })
+	  }
+	}
+	.coordinateSpace(name: scrollSpace)
+	.onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+	  scrollOffset = value
+	}
+  }
 }
