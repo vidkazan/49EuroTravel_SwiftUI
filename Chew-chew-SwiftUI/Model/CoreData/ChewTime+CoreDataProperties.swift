d46aa52902627ea2e1cf41b4bc2bdbea7545961e
Dmitrii Grigorev <vid.kazan@gmail.com>
2023-12-12 20:23:22 +0100

    -coreData: added timeContainer - follow: showing timeContainer and stops

---
+++ Chew-chew-SwiftUI/Model/CoreData/ChewTime+CoreDataProperties.swift
@@ -0,0 +1,84 @@
+//
+//  ChewUser+CoreDataProperties.swift
+//  Chew-chew-SwiftUI
+//
+//  Created by Dmitrii Grigorev on 21.11.23.
+//
+//
+
+import Foundation
+import CoreData
+
+extension ChewTime {
+	@NSManaged public var actualArrival : String?
+	@NSManaged public var actualDeparture : String?
+	@NSManaged public var plannedArrival : String?
+	@NSManaged public var plannedDeparture : String?
+	@NSManaged public var cancelled : Bool
+	
+	@NSManaged public var savedJourney : SavedJourney?
+}
+
+extension ChewTime {
+	static func updateWith(
+		container : TimeContainer,
+		isCancelled : Bool,
+		using managedObjectContext: NSManagedObjectContext,
+		chewTime : ChewTime?
+	) {
+		guard let chewTime = chewTime else {return}
+		chewTime.actualArrival = container.iso.arrival.actual
+		chewTime.plannedArrival = container.iso.arrival.planned
+		chewTime.actualDeparture = container.iso.departure.actual
+		chewTime.plannedDeparture = container.iso.departure.planned
+		chewTime.cancelled = isCancelled
+		do {
+			try managedObjectContext.save()
+		} catch {
+			let nserror = error as NSError
+			print("📕 > save ChewTime: fialed to update ChewTime", nserror.localizedDescription)
+		}
+	}
+
+	static func createWith(
+		container : TimeContainer,
+		using managedObjectContext: NSManagedObjectContext,
+		for savedJourney : SavedJourney,
+		isCancelled : Bool
+	) {
+		let time = ChewTime(
+			context: managedObjectContext,
+			container: container,
+			cancelled: isCancelled
+		)
+		time.savedJourney = savedJourney
+		do {
+			try managedObjectContext.save()
+		} catch {
+			let nserror = error as NSError
+			print("📕 > save ChewTime: fialed to save new ", nserror.localizedDescription)
+		}
+	}
+
+	static func basicFetchRequest(context : NSManagedObjectContext) -> TimeContainer {
+		if let res = fetch(context: context) {
+			return TimeContainer(chewTime: res)
+		}
+		ChewUser.createWith(date: .now, using: context)
+		return TimeContainer(chewTime: fetch(context: context))
+	}
+
+	static private func fetch(context : NSManagedObjectContext) -> ChewTime? {
+		do {
+			let res = try context.fetch(.init(entityName: "ChewTime")).first as? ChewTime
+			if let res = res {
+				return res
+			}
+			print("📕 > basicFetchRequest ChewTime: context.fetch: result is empty")
+			return nil
+		} catch {
+			print("📕 > basicFetchRequest ChewTime: context.fetch error")
+			return nil
+		}
+	}
+}
